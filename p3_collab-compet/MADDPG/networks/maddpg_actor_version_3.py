8e9f050a16f13ddd2c03231b08873604bb9ad929
Brandon Lin <brandon.hy.lin.0@gmail.com>
2019-04-09 22:01:06 +0800

    feat: sync from udacity workspace
    Sync from udacity workspace.

---
+++ p3_collab-compet/MADDPG/networks/maddpg_actor_version_3.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from networks.network_utils import hidden_init
+
+class MADDPGActorVersion3(nn.Module):
+    def __init__(self, state_size, action_size, fc1_units, fc2_units):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super().__init__()
+        
+#         self.seed = torch.manual_seed(seed)
+
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+       
+    
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.relu(self.fc1(state))
+        x = self.bn1(x)
+        x = F.relu(self.fc2(x))
+        
+        return torch.tanh(self.fc3(x))
